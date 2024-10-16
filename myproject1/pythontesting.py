42373e82b3e0b5c42dadd71071cdc60783d062c6
Joshua Petrikat <petrikatjosh@gmail.com>
2024-10-15 17:18:21 -0700

    first commit

---
+++ myproject1/pythontesting.py
@@ -0,0 +1,13 @@
+def distance(x, y, p = 2):
+    
+    result = sum(abs(x - y) ** (p)) ** (1/p)
+    
+    """calculates Lp distance between point x and point y
+    Args:
+        x (np.ndarray): datapoint x
+        y (np.ndarray): datapoint y
+        p (int): order of Lp norm
+    """
+    return result
+
+print(distance(5, 2, p = 2))
